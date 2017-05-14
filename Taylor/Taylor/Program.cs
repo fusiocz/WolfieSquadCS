0213419c1959151910d81cff2d8d750a2f206f76
fusiocz <radek.bystry@gmail.com>
2017-05-14 20:48:23 +0200

    Add files via upload

---
+++ Taylor/Taylor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taylor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            // s pevně zadanou přesností
+            double y = Taylor.Exp(x);
+            Console.WriteLine();
+            Console.WriteLine("exp({0}) = {1, 20:F10}", x, y);
+            Console.WriteLine();
+
+            // s požadovanou přesností
+            y = Taylor.Exp(x, 0.0000001);
+            Console.WriteLine();
+            Console.WriteLine("exp({0}) = {1, 20:F10}", x, y);
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.Write("Zadej uhel (ve °): ");
+            double uhel = double.Parse(Console.ReadLine());
+
+            // s požadovanou přesností
+            // pro jednoduchost: x = 30° = pi*30°/180°
+            x = Math.PI * uhel / 180.0;
+
+            y = Taylor.Sinus(x, 0.0000001);
+            Console.WriteLine();
+            Console.WriteLine("sin({0}°) = {1, 20:F10}", uhel, y);
+            Console.WriteLine();
+
+            Console.WriteLine("stiskni ENTER");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Taylor/Taylor/Taylor.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taylor
+{
+    class Taylor
+    {
+        // metody třídy uvedeny jako:
+        // - public => veřejně přístupné, jinak by se nedaly zavolat
+        // - static => není potřeba před zavoláním metod vytvářet objekt, stačí pouze metody zavolat
+        //              (jako se volá např. Math.Pow())
+
+        // pevná přesnost výpočtu - 0,001
+        public static double Exp(double x)
+        {
+            double rozdil;
+            double clenPosledni = 1;        // 0. člen posloupnosti (n = 0)
+            double soucetPosledni = clenPosledni;
+
+            double clenNovy, soucetNovy;
+            int n = 1;
+
+            clenNovy = x / n;                 // 1. člen posloupnosti (n = 1)
+            soucetNovy = soucetPosledni + clenNovy;
+
+            rozdil = Math.Abs(soucetNovy - soucetPosledni);
+
+            Console.WriteLine();
+            Console.WriteLine("{0, 6} | {1, 20:F10} | {2, 20:F10} | {3, 20:F10}", n, clenNovy, soucetNovy, rozdil);
+
+            clenPosledni = clenNovy;
+            soucetPosledni = soucetNovy;
+
+            n++;
+
+            do
+            {
+                clenNovy = clenPosledni * x / n;
+                soucetNovy = soucetPosledni + clenNovy;
+
+                rozdil = Math.Abs(soucetNovy - soucetPosledni);
+
+                Console.WriteLine();
+                Console.WriteLine("{0, 6} | {1, 20:F10} | {2, 20:F10} | {3, 20:F10}", n, clenNovy, soucetNovy, rozdil);
+
+                clenPosledni = clenNovy;
+                soucetPosledni = soucetNovy;
+
+                n++;
+            } while (rozdil > 0.001);
+
+            return soucetPosledni;
+        }
+
+        // libovolná přesnost výpočtu
+        public static double Exp(double x, double presnost)
+        {
+            double rozdil;
+            double clenPosledni = 1;        // 0. člen posloupnosti (n = 0)
+            double soucetPosledni = clenPosledni;
+
+            double clenNovy, soucetNovy;
+            int n = 1;
+
+            clenNovy = x / n;                 // 1. člen posloupnosti (n = 1)
+            soucetNovy = soucetPosledni + clenNovy;
+
+            rozdil = Math.Abs(soucetNovy - soucetPosledni);
+
+            Console.WriteLine();
+            Console.WriteLine("{0, 6} | {1, 20:F10} | {2, 20:F10} | {3, 20:F10}", n, clenNovy, soucetNovy, rozdil);
+
+            clenPosledni = clenNovy;
+            soucetPosledni = soucetNovy;
+
+            n++;
+
+            do
+            {
+                clenNovy = clenPosledni * x / n;
+                soucetNovy = soucetPosledni + clenNovy;
+
+                rozdil = Math.Abs(soucetNovy - soucetPosledni);
+
+                Console.WriteLine();
+                Console.WriteLine("{0, 6} | {1, 20:F10} | {2, 20:F10} | {3, 20:F10}", n, clenNovy, soucetNovy, rozdil);
+
+                clenPosledni = clenNovy;
+                soucetPosledni = soucetNovy;
+
+                n++;
+            } while (rozdil > presnost);
+
+            return soucetPosledni;
+        }
+
+        // libovolná přesnost výpočtu
+        public static double Sinus(double x, double presnost)
+        {
+            double rozdil;
+            double clenPosledni = x;        // 0. člen posloupnosti (n = 0)
+            double soucetPosledni = clenPosledni;
+
+            double clenNovy, soucetNovy;
+            int n = 1;
+
+            do
+            {
+                // u jednotlivých členů se střídají znaménka + a -
+                clenNovy = -clenPosledni * Math.Pow(x, 2) / ((2 * n + 0) * (2 * n + 1));
+                soucetNovy = soucetPosledni + clenNovy;
+
+                rozdil = Math.Abs(soucetNovy - soucetPosledni);
+
+                Console.WriteLine();
+                Console.WriteLine("{0, 6} | {1, 20:F10} | {2, 20:F10} | {3, 20:F10}", n, clenNovy, soucetNovy, rozdil);
+
+                clenPosledni = clenNovy;
+                soucetPosledni = soucetNovy;
+
+                n++;
+            } while (rozdil > presnost);
+
+            return soucetPosledni;
+        }
+
+    }
+}
